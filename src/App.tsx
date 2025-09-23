ffc2e426f16bc47eadc861da3a41e40fb59f9240
marumaru2018 <mm21wisdom@gmail.com>
2025-09-23 10:10:18 +0900

    Header/ Footerの追加
    - Header コンポーネントの追加
    - Footer コンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       {/* Hero */}
       <div>Hero</div>
       <Hero />
       {/* About */}
       <div>About</div>
       <About />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="py-6 px-4 bg-stone-50">
+      <div className="container mx-auto text-center text-stone-600">
+        <p>
+          Created by{" "}
+          <a
+            className="text-emerald-600"
+            href="#"
+            target="_blank"
+            rel="noreferrer"
+          >
+            marumaru2018
+          </a>{" "}
+          &copy; {year}
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
